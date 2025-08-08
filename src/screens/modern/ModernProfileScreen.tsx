a892569eb29e897fc53e2f912095c8a70190b885
Marc Minott <marcminott@gmail.com>
2025-08-08 07:14:16 -0400

    Add EAS Update debug info component

--- src/screens/modern/ModernProfileScreen.tsx
+++ src/screens/modern/ModernProfileScreen.tsx
@@ -34,6 +34,7 @@ import { NavigationHelper } from '../../services/navigation/NavigationHelper';
 
 // Components
 import { DeveloperSection } from '../../components/developer/DeveloperSection';
+import { UpdateDebugInfo } from '../../components/debug/UpdateDebugInfo';
 import { AnimatedStatsGrid } from '../../components/profile/AnimatedStatsGrid';
 import { AchievementSystem } from '../../components/profile/AchievementSystem';
 import { ActivityTimeline } from '../../components/profile/ActivityTimeline';
@@ -519,6 +520,9 @@ const ModernProfileScreen: React.FC = memo(() => {
             />
           }
         >
+          {/* EAS Update Debug Info - TEMPORARY */}
+          <UpdateDebugInfo />
+          
           {/* Profile Header Card */}
           <LinearGradient
             colors={['#6366F1', '#8B5CF6']}
